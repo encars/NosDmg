cab67a64a9a646c438b81feae466e565c2eabcd6
ILoID <enric.carstens@gmail.com>
2021-03-27 15:17:35 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,16 +1,17 @@
 from injector import Injector
 import psutil
+import os
 
 
 PROCESS_NAME = 'NostaleClientX.exe'
 
 
-def get_packet_logger_path(nostale_pid):
+def getPacketLoggerPath(nostale_pid):
     process = psutil.Process(nostale_pid)
     return os.path.dirname(process.exe()) + '/PacketLogger.dll'
 
 
-def get_pid():
+def getPid():
     for proc in psutil.process_iter():
         try:
             if PROCESS_NAME in proc.name():
@@ -23,8 +24,8 @@ def get_pid():
 def inject():
     injector = Injector()
 
-    pid = get_pid()
-    path_dll = r'C:\Program Files (x86)\Nostale_DE\PacketLogger.dll'
+    pid = getPid()
+    path_dll = getPacketLoggerPath(pid)
 
     injector.load_from_pid(pid)
     injector.inject_dll(path_dll)
